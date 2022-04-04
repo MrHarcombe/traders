221302f23313a84d4d47dc3ca1f986ec9e4b1b0f
hbaig745 <94379637+hbaig745@users.noreply.github.com>
2022-04-04 17:04:17 +0100

    Updated loans tab
    Allows you to pay off loans
    Added refresh button
    Handles errors better now

--- tabs/loans.py
+++ tabs/loans.py
@@ -8,6 +8,7 @@ widgets = {}
 
 MY_LOANS = "https://api.spacetraders.io/my/loans"
 AVAILABLE_LOANS = "https://api.spacetraders.io/types/loans"
+PAY_OFF_LOAN = 'f"https://api.spacetraders.io/my/loans/{loanId}"'
 
 proxy_workaround = {"proxies": {"http": None, "https": None}}
 # proxy_workaround = {"proxies": {"https": "http://127.0.0.1:8080"}, "verify": False}
@@ -19,6 +20,50 @@ def take_out_loan(*args):
         params={"token": trader_token.get(), "type": "STARTUP"},
         **proxy_workaround
     )
+
+    if response.status_code == 422:
+        error_message(response.json()["error"]["message"])
+
+
+def pay_off_loan(*args):
+
+    result = requests.get(
+        MY_LOANS, params={"token": trader_token.get()}, **proxy_workaround
+    )
+
+    try:
+
+        loanId = result.json()["loans"][0]["id"]
+
+        pay_loan = eval(PAY_OFF_LOAN)
+
+        response = requests.put(
+            pay_loan, params={"token": trader_token.get(), "loanId": loanId}
+        )
+
+        if response.status_code == 400:
+            error_message(response.json()["error"]["message"])
+
+    except IndexError:
+        error_message("You dont have any loans to pay off.")
+
+
+def error_message(message):
+
+    root = tk.Tk()
+    root.title("Error Message")
+
+    root.geometry("300x100")
+    root.resizable(height=False, width=False)
+
+    frme = ttk.Frame(root)
+    frme.pack(expand=tk.YES)
+
+    ttk.Label(frme, text=message).pack(expand=tk.YES)
+
+    ttk.Button(frme, text="OK", command=root.destroy).pack(expand=tk.YES)
+
+    root.mainloop()
 
 
 def refresh_loans(*args):
@@ -56,8 +101,6 @@ def refresh_loans(*args):
     except ConnectionError as ce:
         print(ce)
 
-    print(response)
-
     try:
         response = requests.get(
             AVAILABLE_LOANS, params={"token": trader_token.get()}, **proxy_workaround
@@ -86,15 +129,15 @@ def refresh_loans(*args):
     except ConnectionError as ce:
         print(ce)
 
-    print(response)
-
 
 def create_loans_tab(parent, login_token):
     global trader_token
     trader_token = login_token
 
+    main_loan_frame = ttk.Frame(parent)
+
     availableLoansFrame = ttk.LabelFrame(
-        parent, text="Available Loans", relief="groove", padding=5
+        main_loan_frame, text="Available Loans", relief="groove", padding=5
     )
 
     available_loans_table = ttk.Treeview(
@@ -118,12 +161,12 @@ def create_loans_tab(parent, login_token):
     availableLoans_scroll.grid(column=1, row=0, sticky=tk.NS)
     available_loans_table["yscrollcommand"] = availableLoans_scroll.set
 
-    ttk.Button(parent, text="Take out loan", command=take_out_loan).grid(
+    ttk.Button(main_loan_frame, text="Take out loan", command=take_out_loan).grid(
         column=0, row=1, sticky=tk.EW
     )
 
     currentLoansFrame = ttk.LabelFrame(
-        parent, text="Current Loans", relief="groove", padding=5
+        main_loan_frame, text="Current Loans", relief="groove", padding=5
     )
 
     # current loans
@@ -150,9 +193,15 @@ def create_loans_tab(parent, login_token):
     currentLoans_scroll.grid(column=1, row=0, sticky=tk.NS)
     current_loans_table["yscrollcommand"] = currentLoans_scroll.set
 
-    ttk.Button(parent, text="Pay off loan", command=take_out_loan).grid(
+    ttk.Button(main_loan_frame, text="Pay off loan", command=pay_off_loan).grid(
         column=1, row=1, sticky=tk.EW
     )
+
+    ttk.Button(parent, text="Refresh Loans", command=refresh_loans).grid(
+        column=0, row=1, sticky=tk.NSEW
+    )
+
+    main_loan_frame.grid(column=0, row=0, sticky=tk.NSEW)
 
     availableLoansFrame.columnconfigure(0, weight=10)
     availableLoansFrame.columnconfigure(1, weight=1)
@@ -161,20 +210,15 @@ def create_loans_tab(parent, login_token):
     currentLoansFrame.columnconfigure(1, weight=1)
     currentLoansFrame.rowconfigure(0, weight=1)
 
+    main_loan_frame.columnconfigure(0, weight=1)
+    main_loan_frame.columnconfigure(1, weight=1)
+    main_loan_frame.rowconfigure(0, weight=1)
+
     parent.columnconfigure(0, weight=1)
-    parent.columnconfigure(1, weight=1)
-    parent.rowconfigure(0, weight=1)
+    parent.rowconfigure(0, weight=12)
+    parent.rowconfigure(1, weight=1)
 
 
 if __name__ == "__main__":
 
-    root = tk.Tk()
-    frme = ttk.Frame(root)
-
-    create_loans_tab(frme, "e606ddc8-3695-4696-a956-6329033a50b7")
-    # create_loans_tab(frme, login.widgets["trader_token"])
-    frme.pack()
-    # refresh_loans()
-    take_out_loan()
-
-    root.mainloop()
+    error_message("pretend this is an error message")
